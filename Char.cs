a279858d0c8615ed29862dcced1e65e7ff9d3409
insu <insu12345_@naver.com>
2023-02-02 00:39:02 +0900

    [Update] Char Damaged, Move Virus

--- Char.cs
+++ Char.cs
@@ -1,36 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Char : MonoBehaviour
 {
-    public float speed = 0.01f;
+    public float speed = 0.5f;
+    public float maxVelocityX = 10f;
+    public float maxVelocityY = 10f;
     private Rigidbody2D rigid;
+    Animator anim;
+    bool spaceCK = true;
+    public float hp;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        hp = 1f;
     }
 
     // Update is called once per frame
 
-    void FixedUpdate()
+
+    void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        
+        if (Input.GetMouseButtonDown(0))
         {
-            rigid.AddForce(new Vector2(0, speed));
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPos, Camera.main.transform.forward);
+            if (hitInformation.collider != null)
+            {
+                //We should have hit something with a 2D Physics collider!
+                GameObject touchedObject = hitInformation.transform.gameObject;
+                
+                Debug.Log(touchedObject.tag);
+            }
+        }
+
+                if (Input.GetKey(KeyCode.W))
+        {
+            if(rigid.velocity.y < maxVelocityY)
+            {
+                rigid.AddForce(new Vector2(0, 300) * Time.deltaTime);
+            }
+            
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            rigid.AddForce(new Vector2(-speed, 0));
+            if(rigid.velocity.x > maxVelocityX * -1)
+            {
+                rigid.AddForce(new Vector2(-300, 0) * Time.deltaTime);
+            }
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            rigid.AddForce(new Vector2(0, -speed));
+            if(rigid.velocity.y > maxVelocityY * -1)
+            {
+                rigid.AddForce(new Vector2(0, -300) * Time.deltaTime);
+            }
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rigid.AddForce(new Vector2(speed, 0));
+            if(rigid.velocity.x < maxVelocityX)
+            {
+                rigid.AddForce(new Vector2(300, 0) * Time.deltaTime);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && spaceCK)
+        {
+            anim.SetBool("Attack", true);
+            spaceCK = false;
+        } 
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Attack", false);
+            spaceCK = true;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Virus")
+        {
+            Debug.Log("병원균과 접촉하였습니다.");
+            hp -= 0.1f;
+            
         }
     }
 }
